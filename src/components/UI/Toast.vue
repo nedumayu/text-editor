9c69d8c1cf8827e4d7ea18b220cc9cfc33f815ab
nedumayu <nedumayu@gmail.com>
2023-02-25 00:12:59 +0300

    add toast

---
+++ src/components/UI/Toast.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="toast" v-if="show">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toast",
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.toast {
+  position: fixed;
+  top: 20px;
+  right: 0;
+  width: 200px;
+  background: rgba(13, 13, 13, 0.87);
+  color: white;
+  padding: 20px;
+}
+</style>
--- src/components/UI/ui.js
+++ src/components/UI/ui.js
@@ -2,10 +2,12 @@ import Button from "./Button.vue";
 import Input from "./Input.vue";
 import Modal from "./Modal.vue";
 import Select from "./Select.vue";
+import Toast from "./Toast.vue";
 
 export default [
     Button,
     Input,
     Modal,
-    Select
+    Select,
+    Toast
 ]
