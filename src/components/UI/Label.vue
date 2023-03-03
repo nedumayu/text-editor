bdc7bc5e2fb7933b85059603a508018872d49451
nedumayu <nedumayu@gmail.com>
2023-03-03 19:39:16 +0300

    add label UI

---
+++ src/components/UI/Label.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="label">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Label"
+}
+</script>
+
+<style scoped>
+.label {
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
--- src/components/UI/ui.js
+++ src/components/UI/ui.js
@@ -3,10 +3,12 @@ import Input from "./Input.vue";
 import Modal from "./Modal.vue";
 import Select from "./Select.vue";
 import Toast from "./Toast.vue";
+import Label from "./Label.vue";
 
 export default [
     Button,
     Input,
+    Label,
     Modal,
     Select,
     Toast
