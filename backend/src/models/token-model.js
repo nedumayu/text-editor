6cfad351c0bb8c47f472d9032672872db4886ea5
Alice <nedumayu@gmail.com>
2023-03-19 21:08:47 +0300

    add token functions and fix model

--- backend/src/models/token-model.js
+++ backend/src/models/token-model.js
@@ -2,7 +2,7 @@ import {model, Schema} from "mongoose"
 
 const TokenModel = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
-    token: {type: String, required: true},
+    refreshToken: {type: String, required: true},
 })
 
 export default model('Token', TokenModel)
--- backend/src/services/token-service.js
+++ backend/src/services/token-service.js
@@ -3,7 +3,7 @@ import TokenModel from "../models/token-model.js"
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '15m'})
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '20m'})
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
         return {
             accessToken,
@@ -19,6 +19,34 @@ class TokenService {
         }
         const token = await TokenModel.create({user: userId, refreshToken})
         return token
+    }
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+            return userData
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+            return userData
+        } catch (e) {
+            return null;
+        }
+    }
+
+    async removeToken(refreshToken) {
+        const tokenData = await TokenModel.deleteOne({refreshToken})
+        return tokenData
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await TokenModel.findOne({refreshToken})
+        return tokenData
     }
 }
 
