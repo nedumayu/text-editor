1989bbcaaffff1e0029946072de60d4e2b508e8f
Alice <nedumayu@gmail.com>
2023-03-19 21:09:29 +0300

    add login, refresh and logout functions

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,39 +1,58 @@
 import UserService from "../services/user-service.js";
+import {validationResult} from "express-validator";
+import ApiError from "../exceptions/api-errors.js";
+import BoardService from "../services/board-service.js";
 
 class AuthController {
     async registration(req, res, next) {
-        try{
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибки при валидации', errors.array()))
+            }
             const {email, password, username} = req.body
             const userData = await UserService.registration(email, password, username)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData)
         } catch (e) {
-            console.log(e)
+            next(e)
         }
     }
 
     async login(req, res, next) {
-        try{
+        try {
+            const {email, password} = req.body
+            const userData = await UserService.login(email, password)
 
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
         } catch (e) {
-
+            next(e)
         }
     }
 
     async logout(req, res, next) {
-        try{
-
+        try {
+            const {refreshToken} = req.cookies;
+            const token = await UserService.logout(refreshToken)
+            res.clearCookie('refreshToken')
+            return res.json(token)
         } catch (e) {
-
+            next(e)
         }
     }
 
     async refresh(req, res, next) {
-        try{
-
+        try {
+            const {refreshToken} = req.cookies;
+            const userData = await UserService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
         } catch (e) {
-
+            next(e)
         }
     }
+    
 }
 
 export default new AuthController()
--- backend/src/services/user-service.js
+++ backend/src/services/user-service.js
@@ -2,7 +2,7 @@ import UserModel from "../models/user-model.js"
 import bcrypt from 'bcrypt'
 import ApiError from "../exceptions/api-errors.js"
 import UserDto from "../dtos/user-dto.js"
-import TokenService from "./token-service.js";
+import TokenService from "./token-service.js"
 
 class UserService {
     async registration(email, password, username) {
@@ -16,8 +16,50 @@ class UserService {
         const userDto = new UserDto(user);
         const tokens = TokenService.generateTokens({...userDto})
         await TokenService.saveToken(userDto.id, tokens.refreshToken)
+        return {...tokens, user: userDto}
+    }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({email})
+        if (!user) {
+            throw ApiError.BadRequest('Пользователь не зарегистрирован')
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if(!isPassEquals) {
+            throw ApiError.BadRequest('Неверный пароль')
+        }
+        const userDto = new UserDto(user);
+        const tokens = TokenService.generateTokens({...userDto})
+        await TokenService.saveToken(userDto.id, tokens.refreshToken)
 
         return {...tokens, user: userDto}
+    }
+
+    async logout(refreshToken) {
+        const token = await TokenService.removeToken(refreshToken)
+        return token;
+    }
+
+    async refresh(refreshToken) {
+        if(!refreshToken) {
+            throw ApiError.UnauthorizedError()
+        }
+        const userData = TokenService.validateRefreshToken(refreshToken)
+        const tokenFromDb = await TokenService.findToken(refreshToken)
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError()
+        }
+        const user = await UserModel.findById(userData.id)
+        const userDto = new UserDto(user);
+        const tokens = TokenService.generateTokens({...userDto})
+        await TokenService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {...tokens, user: userDto}
+    }
+
+    async getUsers() {
+        const users = await UserModel.find()
+        return users
     }
 }
 
