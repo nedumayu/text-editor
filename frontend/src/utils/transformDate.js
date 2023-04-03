eda092e255cf7696e4a30316dae6d405aa456dbd
Alice <nedumayu@gmail.com>
2023-04-03 22:44:28 +0300

    add tailwind configuration

--- frontend/src/utils/transformDate.js
+++ frontend/src/utils/transformDate.js
@@ -1,4 +1,4 @@
 export default function transformDate (date) {
-    return date.slice(8, 10) + ":" + date.slice(5, 7) + ":" + date.slice(0, 4) +
-        " " + (Number(date.slice(11, 13)) + 3) + ":" + date.slice(14, 16) + ":" + date.slice(17, 19)
+    return date.slice(8, 10) + "/" + date.slice(5, 7) + "/" + date.slice(0, 4) +
+        "  " + (Number(date.slice(11, 13)) + 3) + ":" + date.slice(14, 16) + ":" + date.slice(17, 19)
 }
