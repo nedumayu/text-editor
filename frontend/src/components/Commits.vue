6c2f5dd18c280d0696d64a6595455b8ab9c42da8
nedumayu <nedumayu@gmail.com>
2023-04-06 22:31:00 +0300

    reverse changes array and slice

--- frontend/src/components/Commits.vue
+++ frontend/src/components/Commits.vue
@@ -3,17 +3,17 @@
     <h4 class="mb-3">COMMITS:</h4>
     <div class="flex flex-col space-y-1">
       <div
-          v-for="change in changes"
+          v-for="change in [...changes].reverse().slice(0,4)"
           :key="change.id"
-          class="flex bg-secondary-focus rounded-xl px-3 py-1 space-x-4"
+          class="flex bg-secondary-focus rounded-xl px-3 py-1"
       >
         <strong>
           {{ change.user.username }}
         </strong>
-        <div>
+        <div class="w-44 ml-3">
           {{ transformDate(change.date)}}
         </div>
-        <div class="whitespace-nowrap	overflow-hidden max-w-54 text-ellipsis" :title="`${change.content}`">{{ change.content }}</div>
+        <div class="whitespace-nowrap	overflow-hidden w-[168px] text-ellipsis" :title="`${change.content}`">{{ change.content }}</div>
       </div>
     </div>
   </div>
